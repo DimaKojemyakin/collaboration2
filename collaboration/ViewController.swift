401194e1442e2d56c1c5aca1607f0de969ee39da
dima <dima@kojem.ru>
2023-10-03 21:38:44 +0300

    Initial Commit

---
+++ collaboration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collaboration
+//
+//  Created by Дима Кожемякин on 03.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
